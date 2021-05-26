6e7f01792a4d803a4023d5b24d5f60f3b670f0f3
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-26 15:52:55 -0500

    Updated button output

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const generateListButton = document.querySelector('#generateList');
+const list = document.querySelector('#myList');
+const inputs = document.querySelectorAll('.input');
+
+generateListButton.addEventListener('click', function () {
+    list.innerHTML = "";
+    const h1element = document.createElement(`h1`);
+    h1element.innerText = (`My Shopping List`);
+    list.append(h1element);
+    inputs.forEach(function(item) {
+        const listItem = document.createElement('ul');
+        listItem.innerText = item.value;
+        list.append(listItem);
+    });
+});
